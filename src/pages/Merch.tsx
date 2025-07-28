c265ab604ca7406833d287bdb616e8d5901b2fed
Adam Swedlund <aswedlund12@gmail.com>
2025-07-28 18:44:16 -0500

    added link to learn about points on merch page

--- src/pages/Merch.tsx
+++ src/pages/Merch.tsx
@@ -2,6 +2,7 @@ import Navbar from '../components/Navbar';
 import MerchGrid from '../components/merch-page/merch-grid/MerchGrid';
 import Header from '../components/merch-page/header/Header';
 import Footer from '../components/footer/Footer';
+import Payment from '../components/merch-page/payment/Payment';
 
 function Merch() {
     return (
@@ -9,6 +10,7 @@ function Merch() {
             <Navbar page="Merch" />
             <Header />
             <MerchGrid />
+            <Payment />
             <Footer />
         </>
     );
